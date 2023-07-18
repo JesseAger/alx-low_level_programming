b972242a33462d6ab0b1de1022b4c93b5d88e8d7
JesseAger <agerjesse6@gmail.com>
2023-07-18 19:43:18 +0300

    alphabetC

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void print_alphabet(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        putchar(c);
+    }
+
+    putchar('\n');
+}
+
+int main()
+{
+    print_alphabet();
+    return 0;
+}
